0e00591060164933faa7012a36692c2c1cfa32be
akhras <abdoakhras4@gmail.com>
2024-02-09 20:59:16 +0100

    add interupt

--- PomodoroClock.js
+++ PomodoroClock.js
@@ -1,34 +1,36 @@
-function work(input1,input2) {
+function work(input1,input2,pro) {
   let mintolsec = input1 * 60;
   let timer = setInterval(() => {
     let min = Math.floor(mintolsec / 60);
     let sec = mintolsec % 60;
-
-    console.log(`${min} : ${sec} `);
-
-    if (mintolsec === 0) {
+    console.log(`${min} : ${sec}`);
+    if (mintolsec == 0) {
       clearInterval(timer); 
-        statRest(input2);
-    } else {
+      statRest(input2,input1);
+    }else if(pro){
+      clearInterval(timer);
+    }else {
       mintolsec--; 
     }
   }, 1000); 
 }
-function statRest(input2){
+function statRest(input2,input1){
   let mintolsec = input2 * 60;
   let timer = setInterval(() => {
     let min = Math.floor(mintolsec / 60);
     let sec = mintolsec % 60;
     console.log(`${min} : ${sec} `);
  
-  if (mintolsec === input2) {
+  if (mintolsec == 0) {
     clearInterval(timer); 
-      work(input2);
-  } else {
-    mintolsec++; 
+    work(input1,input2);
+  }else if(pro){
+    clearInterval(timer); 
+  }else {
+    mintolsec--; 
   }
 }, 1000);
 }
 
 
-work(1,0.5);
+work(1,0.5,false);
