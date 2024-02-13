a00cb4eeb6479b224650e22b50a912cb21dd2994
akhras <abdoakhras4@gmail.com>
2024-02-13 15:56:05 +0100

    Co-authored-by: M-Hamza-1<M-Hamza-1@users.noreply.github.com>

--- PuttingItAll.js
+++ PuttingItAll.js
@@ -1,3 +1,4 @@
+/* The above class defines a Person with a name and age, and has a method to introduce themselves. */
 class Person {
 constructor(name,age){
     this.name=name;
@@ -11,12 +12,18 @@ introduce(){
 const PrintUser=new Person("Johan",25)
 const Person1=new Person("tom",50)
 PrintUser.introduce();
+/**
+ * The function describePerson calls the introduce method of the Person1 object.
+ * @param callback - The callback parameter is a function that will be called within the describePerson
+ * function.
+ */
 function describePerson(callback){
     callback.call(Person1);
 }
 describePerson(Person1.introduce)
 
 
+/* The code snippet is creating a Promise object called `wait`. */
 let number=20
 let wait = new Promise((resolve,reject)=>{
     if(number){
